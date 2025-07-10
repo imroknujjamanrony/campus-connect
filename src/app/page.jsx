2ab46170c22bc910092aa00ebe5af55ec1d2c9c8
Md Roknujjaman Rony <roknujjamanrony1234@gmail.com>
2025-07-10 10:25:18 +0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Footer from "@/components/shared/Footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Footer></Footer>
+    </div>
+  );
+}
---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>
+        &copy; {new Date().getFullYear()} Campus Connect. All rights reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
