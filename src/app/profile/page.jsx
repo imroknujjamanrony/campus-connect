07235aad4e688763f4339bfff0be9f1ec68d4d92
imroknujjamanrony <roknujjamanrony1234@gmail.com>
2025-09-22 22:38:01 +0600

    modified the college page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -3,6 +3,7 @@ export default function ProfilePage() {
     <div>
       <h1>profile page</h1>
       <h2>heloo bangladesh</h2>
+      <h2>hellow bangladesh ami tomake valobasi</h2>
     </div>
   );
 }
